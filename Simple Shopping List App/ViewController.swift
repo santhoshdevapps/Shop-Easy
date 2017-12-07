7428adb5d13a4ce7d3bc826c4914d70e0c60882b
santhosh damodharan <santhoshdamodharan@santhoshs-macbook-air.local>
2017-12-06 22:19:05 -0500

    - fixes for cancel button - Final commit

--- Simple Shopping List App/ViewController.swift
+++ Simple Shopping List App/ViewController.swift
@@ -1,14 +1,16 @@
 /*
  * File Name : ViewController.swift
- * Name : Santhosh Damodharan
+ * Name : Santhosh Damodharan, Ferando Ito, Aman Preet Kaur
  * Date : 2017-12-06
- * StudentID : 300964037
+ * StudentID : 300964037, 300960367, 300966930
  * Description : Shop easy - A simple shopping online app
  * Version : 1.0
  */
 
 import UIKit
-
+/*
+ This class used to
+ */
 class ViewController: UIViewController {
 
     
@@ -26,6 +28,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var stepperValue3: UILabel!
     @IBOutlet weak var stepperValue4: UILabel!
 
+    @IBOutlet weak var textValue0: UITextField!
+    @IBOutlet weak var textValue1: UITextField!
+    @IBOutlet weak var textValue2: UITextField!
+    @IBOutlet weak var textValue3: UITextField!
+    @IBOutlet weak var textValue4: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
@@ -37,15 +45,15 @@ class ViewController: UIViewController {
      */
     
     @IBAction func stepperAction(_ sender: Any) {
-        stepperValue0.text = "/(Int(stepper0.value))"
-        stepperValue1.text = "/(Int(stepper1.value))"
-        stepperValue2.text = "/(Int(stepper2.value))"
-        stepperValue3.text = "/(Int(stepper3.value))"
-        stepperValue4.text = "/(Int(stepper4.value))"
+        stepperValue0.text = Int(stepper0.value).description
+        stepperValue1.text = Int(stepper1.value).description
+        stepperValue2.text = Int(stepper2.value).description
+        stepperValue3.text = Int(stepper3.value).description
+        stepperValue4.text = Int(stepper4.value).description
     }
 
     /*!
-     * @discussion It resets the Label
+     * @discussion it is used to resets the Label
      */
     
     @IBAction func cancelButton(_ sender: UIButton) {
@@ -54,6 +62,11 @@ class ViewController: UIViewController {
         stepperValue2.text = "0"
         stepperValue3.text = "0"
         stepperValue4.text = "0"
+        textValue0.text = ""
+        textValue1.text = ""
+        textValue2.text = ""
+        textValue3.text = ""
+        textValue4.text = ""
         
     }
 
