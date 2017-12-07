a70ffc66d66a1dd96a1b27eaba188ac5ad2d0e99
santhosh damodharan <santhoshdamodharan@santhoshs-macbook-air.local>
2017-12-06 21:13:39 -0500

    - Added cancel functionalities - Added comments

--- Simple Shopping List App/ViewController.swift
+++ Simple Shopping List App/ViewController.swift
@@ -1,25 +1,61 @@
-//
-//  ViewController.swift
-//  Simple Shopping List App
-//
-//  Created by santhosh damodharan on 2017-12-06.
-//  Copyright © 2017 santhosh damodharan. All rights reserved.
-//
-
+/*
+ * File Name : ViewController.swift
+ * Name : Santhosh Damodharan
+ * Date : 2017-12-06
+ * StudentID : 300964037
+ * Description : Shop easy - A simple shopping online app
+ * Version : 1.0
+ */
 import UIKit
 
 class ViewController: UIViewController {
 
+    // VARIABLES +++++++++++++++++++++++++++
+    
+    
+ // OUTLETS ++++++++++++++++++++++++++++++
+    @IBOutlet weak var stepper0: UIStepper!
+    @IBOutlet weak var stepper1: UIStepper!
+    @IBOutlet weak var stepper2: UIStepper!
+    @IBOutlet weak var stepper3: UIStepper!
+    @IBOutlet weak var stepper4: UIStepper!
+    @IBOutlet weak var stepperValue0: UILabel!
+    @IBOutlet weak var stepperValue1: UILabel!
+    @IBOutlet weak var stepperValue2: UILabel!
+    @IBOutlet weak var stepperValue3: UILabel!
+    @IBOutlet weak var stepperValue4: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // ACTIONS AND METHODS
+    
+    /*!
+     * @discussion It is used to get the user input as number and display in Labels
+     */
+    
+    @IBAction func stepperAction(_ sender: Any) {
+        stepperValue0.text = "/(Int(stepper0.value))"
+        stepperValue1.text = "/(Int(stepper1.value))"
+        stepperValue2.text = "/(Int(stepper2.value))"
+        stepperValue3.text = "/(Int(stepper3.value))"
+        stepperValue4.text = "/(Int(stepper4.value))"
     }
-
-
+    
+    /*!
+     * @discussion It resets the Label
+     */
+    
+    @IBAction func cancelButton(_ sender: UIButton) {
+        stepperValue0.text = "0"
+        stepperValue1.text = "0"
+        stepperValue2.text = "0"
+        stepperValue3.text = "0"
+        stepperValue4.text = "0"
+        
+    }
+    
 }
 
